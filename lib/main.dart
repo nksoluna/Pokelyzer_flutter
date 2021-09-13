3686c012faa7631a3bc278f2c0fb61de57787760
Piravit Chenpittaya <karnzx-18762@hotmail.com>
2021-09-14 00:08:37 +0700

    create variable color for reuse

--- lib/main.dart
+++ lib/main.dart
@@ -7,20 +7,22 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  Color primaryColor = const Color(0xFFff2626);
+  Color secondaryColor = const Color(0xFFbd1616);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'LastTime Tracker App',
       theme: ThemeData(
-        primaryColor: const Color(0xFFff2626),
-        accentColor: const Color(0xFFbd1616),
+        primaryColor: primaryColor,
+        accentColor: secondaryColor,
         primarySwatch: Palette.kToDark,
         disabledColor: Colors.grey[600],
         brightness: Brightness.light,
         textTheme: const TextTheme(
           headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
           headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-          bodyText2: TextStyle(fontSize: 16.0, color: Color(0xFF000000)),
+          bodyText2: TextStyle(fontSize: 15.0, color: Color(0xFF000000)),
         ),
       ),
       home: Pokelyzer(),
