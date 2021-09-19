99cad5a66f2610dbf79d4fc88a3e0079bad04b79
nksoluna <60749514+nksoluna@users.noreply.github.com>
2021-09-19 14:34:00 +0700

    update home

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
-import 'pokelyzer.dart';
+import 'package:pokelyzer/Screens/pokemonall/pokemon_all.dart';
+import 'package:pokelyzer/pokemon_info/pokemon_info.dart';
+import 'Helpers/palette.dart';
+import 'Screens/Compare/compare.dart';
+import 'Screens/Home/home.dart';
 
 void main() async {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final Color primaryColor = const Color(0xFFff2626);
+  final Color secondaryColor = const Color(0xFFbd1616);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pokelyzer',
+      title: 'LastTime Tracker App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: primaryColor,
+        accentColor: secondaryColor,
+        primarySwatch: Palette.kToDark,
+        disabledColor: Colors.grey[600],
+        brightness: Brightness.light,
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0, color: Color(0xFF000000)),
+        ),
       ),
-      home: Pokelyzer(),
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => AllpokemonScreen(),
+        '/compare': (context) => CompareScreen(),
+        '/character': (context) => PokemonInfoScreen(),
+      },
     );
   }
 }
